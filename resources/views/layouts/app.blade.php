0102a5665268380c7b70184e64fb190ea03d0a78
mmk <myatmoekyawucsm99@gmail.com>
2024-03-22 16:49:49 +0630

    update table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!--datatables css-->
         <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
        
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,42 +5,36 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                <table id="example" class="display" style="width: 100%;">
-                    <thead class="bg-gray-100 rounded-md">
-                        <tr>
-                            <th class="py-5 text-center">#</th>
-                            <th class="py-5 text-center">Name</th>
-                            <th class="py-5 text-center">Email</th>
-                            <th class="py-5 text-center">Password</th>
-                            <th class="py-5 text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                            <td class="py-3 text-center">1</td>
-                            <td class="py-3 text-center">John Doe</td>
-                            <td class="py-3 text-center">john@example.com</td>
-                            <td class="py-3 text-center">45678</td>
-                            <td class="py-3 text-center">
-                                <button class="btn btn-primary">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>  
-                    </tbody>
-                </table>
-                </div>
-            </div>
-        </div>
+    <div class="mt-3 container-fluid">
+        <table id="example" class="table table-bordered table-striped" style="width: 100%;">
+            <thead class="">
+                <tr>
+                    <th class=" text-center">#</th>
+                    <th class=" text-center ">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class=" text-center">Password</th>
+                    <th class=" text-center">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="py-3 text-center">1</td>
+                    <td class="py-3 text-center">John Doe</td>
+                    <td class="py-3 text-center">john@example.com</td>
+                    <td class="py-3 text-center">45678</td>
+                    <td class="py-3 text-center">
+                        <button class="btn btn-primary">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>  
+            </tbody>
+        </table>
     </div>
 </x-app-layout>
 <script>
     $(document).ready(function() {
         $('#example').DataTable({
-            responsive: true
+            // responsive: true,
         });
     });
 </script>
